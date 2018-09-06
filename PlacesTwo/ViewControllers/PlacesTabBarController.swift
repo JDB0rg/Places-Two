6b95bb1c577aec5e48bdc4593062b1c590474f7a
Jonah Bergevin <jonahbergevin@gmail.com>
2018-09-06 11:18:07 -0600

    added subclass for all storyboards

--- PlacesTwo/ViewControllers/PlacesTabBarController.swift
+++ PlacesTwo/ViewControllers/PlacesTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlacesTabBarController.swift
 //  PlacesTwo
 //
 //  Created by Madison Waters on 9/6/18.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlacesTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
